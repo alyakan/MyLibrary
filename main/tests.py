be8c75fa291ac010520c07a1daf35920cda5b116
Aly Yakan <alyyakan@hotmail.com>
2015-06-12 17:01:40 +0200

    S01 Registration tests

--- main/tests.py
+++ main/tests.py
@@ -8,9 +8,39 @@ class UserTestCase(TestCase):
     def Setup(self):
         self.client = Client()
 
+    def test_ensure_registration_with_not_null_username_password(self):
+        """
+        Results True for Users registering with correct credintials,
+        redirects to index.html
+
+        Author: Aly Yakan
+        """
+        response = self.client.post(
+                        reverse('register'),
+                        {'username': u'markdoe',
+                         'email': u'mark@gmail.com',
+                         'password': u'123456'})
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(User.objects.get(username='markdoe') is None)
+
+    def test_ensure_registration_with_null_username_password(self):
+        """
+        Results False for Users registering with false credintials,
+        redirects to index.html
+
+        Author: Aly Yakan
+        """
+        response = self.client.post(
+                        reverse('register'),
+                        {'username': u'markdoe',
+                         'email': u'mark@gmail.com',
+                         'password': u''})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.all().count(), 0)
+
     def test_ensure_login_with_correct_credintials(self):
-    	"""
-        results True for Users logging in with correct credintials, redirects
+        """
+        Results True for Users logging in with correct credintials, redirects
         to index.html
 
         Author: Aly Yakan
@@ -22,8 +52,8 @@ class UserTestCase(TestCase):
         self.assertEqual(response.status_code, 302)
 
     def test_deny_login_with_wrong_credintials(self):
-    	"""
-        prevents Users logging in with wrong credintials, stays on
+        """
+        Prevents Users logging in with wrong credintials, stays on
         same page
 
         Author: Aly Yakan
