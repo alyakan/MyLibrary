f93cff04169b6c89b7757bb24592f69cdf15403c
Aly Yakan <alyyakan@hotmail.com>
2015-06-12 20:28:16 +0200

    S04 S08 S12 Library Test Cases

--- main/tests.py
+++ main/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase, Client
-from main.models import User
+from main.models import User, Library
 from django.core.urlresolvers import reverse
 
 
@@ -63,4 +63,101 @@ class UserTestCase(TestCase):
         response = self.client.post(
             reverse('login'),
             {'username': u'johndoe', 'password': '4567'})
+        self.assertEqual(response.status_code, 200)
+
+
+class LibraryTestCase(TestCase):
+
+    def Setup(self):
+        self.client = Client()
+
+    def test_ensure_library_created_with_not_null_name(self):
+        """
+        Ensures a library is created with correct information
+
+        Author: Aly Yakan
+        """
+        User.objects.create_user(username='johnnydoe',
+                                 password='123456')
+        response = self.client.post(
+            reverse('login'),
+            {'username': u'johnnydoe', 'password': '123456'})
+        self.assertEqual(response.status_code, 302)
+        user = User.objects.get(username='johnnydoe')
+        response = self.client.post(
+            reverse('library-add'),
+            {'name': 'johnnylibrary',
+             'location': 'here',
+             'owner': user.id})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Library.objects.all().count(), 1)
+        self.assertFalse((Library.objects.get(name='johnnylibrary') is None))
+
+    def test_ensure_library_not_created_with_null_name(self):
+        """
+        Ensures a library is not created with null name
+
+        Author: Aly Yakan
+        """
+        User.objects.create_user(username='johnnydoe',
+                                 password='123456')
+        response = self.client.post(
+            reverse('login'),
+            {'username': u'johnnydoe', 'password': '123456'})
+        self.assertEqual(response.status_code, 302)
+        user = User.objects.get(username='johnnydoe')
+        response = self.client.post(
+            reverse('library-add'),
+            {'name': '',
+             'location': 'here',
+             'owner': user.id})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Library.objects.all().count(), 0)
+
+    def test_list_view_not_signed_in(self):
+    	"""
+        Ensures a guest can browse libraries from
+        libraries' list view
+
+        Author: Aly Yakan
+        """
+        user1 = User.objects.create_user(username='johndoe',
+                                         password='123456')
+        user2 = User.objects.create_user(username='johnnydoe',
+                                         password='123456')
+        response = self.client.post(
+            reverse('library-add'),
+            {'name': u'Cairo Library',
+             'location': u'Cairo',
+             'owner': user1.id})
+        self.assertEqual(response.status_code, 302)
+        response = self.client.post(
+            reverse('library-add'),
+            {'name': u'Texas Library',
+             'location': u'Texas',
+             'owner': user2.id})
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get('/main/library-list/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('object_list' in response.context)
+        self.assertEqual(
+            [Library.pk for Library in response.context['object_list']],
+            [1, 2])
+
+    def test_library_detailed_view_not_signed_in(self):
+    	"""
+        Ensures a guest can access a library's detailed view
+
+        Author: Aly Yakan
+        """
+        user = User.objects.create_user(username='johndoe',
+                                        password='123456')
+        response = self.client.post(
+            reverse('library-add'),
+            {'name': u'Cairo Library',
+             'location': u'Cairo',
+             'owner': user.id})
+        self.assertEqual(response.status_code, 302)
+        lib_slug = Library.objects.get(owner=user.id).slug
+        response = self.client.get('/main/mylibrary/' + lib_slug + '/')
         self.assertEqual(response.status_code, 200)
